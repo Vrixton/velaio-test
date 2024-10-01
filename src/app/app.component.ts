14588597d969653f16ea6a99efe71dd9836428ea
Victor Villavicencio <victor.villavicencio.10@gmail.com>
2024-10-01 13:36:52 -0500

    main structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  toppings = new FormControl('');
   title = 'velaio-test';
+  toppingList: string[] = ['Todas', 'Completadas', 'Pendientes'];
+  tasks = [
+    {
+      name: 'Task 1',
+      limitDate: '12/21/12',
+      status: 'complete',
+      people: [
+        {
+          name: 'People #1',
+          age: 12,
+          skills: ['javascript', 'html']
+        },
+        {
+          name: 'People #2',
+          age: 20,
+          skills: ['angular', 'typescript']
+        }
+      ]
+    },
+    {
+      name: 'Task 2',
+      limitDate: '01/01/24',
+      status: 'pending',
+      people: [
+        {
+          name: 'People #1',
+          age: 12,
+          skills: ['javascript', 'html']
+        },
+        {
+          name: 'People #2',
+          age: 20,
+          skills: ['angular', 'typescript']
+        }
+      ]
+    },
+    {
+      name: 'Task 3',
+      limitDate: '01/01/24',
+      status: 'pending',
+      people: [
+        {
+          name: 'People #1',
+          age: 12,
+          skills: ['javascript', 'html']
+        },
+        {
+          name: 'People #2',
+          age: 20,
+          skills: ['angular', 'typescript']
+        }
+      ]
+    }
+  ]
 }
