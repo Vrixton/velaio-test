394755e576f165166116a4afe55c5850889e24ea
Victor Villavicencio <victor.villavicencio.10@gmail.com>
2024-10-02 20:15:07 -0500

    get task from taskService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,78 +1,41 @@
-import {Component} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {MatDialog, MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
-import {NgIf} from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { NewTaskComponent } from './new-task/new-task.component';
-
+import { TaskService } from './services/task/task.service';
+import { DatePipe } from '@angular/common';
+import { Timestamp } from '@angular/fire/firestore';  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [DatePipe] 
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   toppings = new FormControl('');
   title = 'velaio-test';
   toppingList: string[] = ['Todas', 'Completadas', 'Pendientes'];
-  tasks = [
-    {
-      name: 'Task 1',
-      limitDate: '12/21/12',
-      status: 'complete',
-      people: [
-        {
-          name: 'People #1',
-          age: 12,
-          skills: ['javascript', 'html']
-        },
-        {
-          name: 'People #2',
-          age: 20,
-          skills: ['angular', 'typescript']
-        }
-      ]
-    },
-    {
-      name: 'Task 2',
-      limitDate: '01/01/24',
-      status: 'pending',
-      people: [
-        {
-          name: 'People #1',
-          age: 12,
-          skills: ['javascript', 'html']
-        },
-        {
-          name: 'People #2',
-          age: 20,
-          skills: ['angular', 'typescript']
-        }
-      ]
-    },
-    {
-      name: 'Task 3',
-      limitDate: '01/01/24',
-      status: 'pending',
-      people: [
-        {
-          name: 'People #1',
-          age: 12,
-          skills: ['javascript', 'html']
-        },
-        {
-          name: 'People #2',
-          age: 20,
-          skills: ['angular', 'typescript']
-        }
-      ]
-    }
-  ]
+  tasks: any[] = [];
 
-  constructor(public dialog: MatDialog) {}
+  constructor(
+    public dialog: MatDialog, 
+    private _task: TaskService, 
+    private datePipe: DatePipe) {}
+
+  ngOnInit(): void {
+    this._task.getTasks().subscribe(data => {
+      this.tasks = data;
+      console.log('Tareas obtenidas:', this.tasks);
+    });
+  }
+
+  formatTimestamp(firebaseTimestamp: Timestamp): string | null {
+    const date = firebaseTimestamp.toDate(); 
+    return this.datePipe.transform(date, 'dd/MM/yyyy');
+  }
   
   newTask() {
-    console.log("new task");
     this.dialog.open(NewTaskComponent);
   }
 
